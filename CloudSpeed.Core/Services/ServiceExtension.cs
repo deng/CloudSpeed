cecf1eab62b61b751505b1cbf7ff7f1e6d2c48f8
Derek Deng <deng.zz@qq.com>
2020-10-07 02:46:20 +0800

    add log4net

--- CloudSpeed.Core/Services/ServiceExtension.cs
+++ CloudSpeed.Core/Services/ServiceExtension.cs
@@ -1,10 +1,13 @@
-﻿using CloudSpeed.Managers;
+﻿using System.Linq;
+using CloudSpeed.Managers;
 using CloudSpeed.Powergate;
 using CloudSpeed.Repositories;
 using CloudSpeed.Sdk;
 using CloudSpeed.Web;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace CloudSpeed.Services
 {
@@ -12,6 +15,9 @@ namespace CloudSpeed.Services
     {
         public static IServiceCollection AddAppServices(this IServiceCollection services, IConfiguration configuration)
         {
+            var loggerFactory = LoggerFactory
+                .Create(builder => builder.AddFilter(FilterEntityFrameworkCore).AddLog4Net(configuration.CombineConfigPath("log4net.config")));
+            services.AddSingleton<ILoggerFactory>(loggerFactory);
             services.BindConfiguration(configuration);
             services.AddSingleton<LotusClient>();
             services.UseRepositories();
@@ -19,6 +25,16 @@ namespace CloudSpeed.Services
             services.AddSingleton<CloudSpeedManager>();
             services.UsePowergateRrpcServices(configuration);
             return services;
+        }
+
+        private static bool FilterEntityFrameworkCore(string category, Microsoft.Extensions.Logging.LogLevel level)
+        {
+            var filtedCategories = new[]
+            {
+                DbLoggerCategory.Database.Command.Name,
+                "Microsoft.EntityFrameworkCore.Infrastructure"
+            };
+            return !(filtedCategories.Contains(category) && level == Microsoft.Extensions.Logging.LogLevel.Information);
         }
     }
 }
